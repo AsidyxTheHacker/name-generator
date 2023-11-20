7d540770d27fda1afea903d7bace8b719ddd5ef7
Adam <adammiller.2000nj@gmail.com>
2023-11-20 16:23:11 -0500

    added arrays, added randomNum & added click eventlisteners

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let randomNum = Math.floor(Math.random() * 5);
 let container = document.getElementById('container');
 let nameContainer = document.createElement('div');
 nameContainer.id = 'nameContainer';
@@ -13,5 +14,12 @@ for(i = 0; i < 3; i++){
 }
 
 let textOne = document.querySelector('#nameContainer div:nth-child(1) p.commonText');
+    textOne.addEventListener('click', () => {textOne.innerText = arrayOne[`${randomNum}`];})
 let textTwo = document.querySelector('#nameContainer div:nth-child(2) p.commonText');
+    textTwo.addEventListener('click', () => {textTwo.innerText = arrayTwo[`${randomNum}`];})
 let textThree = document.querySelector('#nameContainer div:nth-child(3) p.commonText');
+    textThree.addEventListener('click', () => {textThree.innerText = arrayThree[`${randomNum}`];})
+
+let arrayOne = ['mr.', 'the', 'dr.', 'king', 'mrs.'];
+let arrayTwo = ['giant', 'smelly', 'cool', 'tiny', 'loud'];
+let arrayThree = ['potato', 'clown', 'rapper', 'squid', 'box'];
